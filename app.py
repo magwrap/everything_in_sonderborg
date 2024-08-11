d93645051093cfa68d7981ab4675c3cb9bb9b3e0
Jan Musiol <jasiek.musiol@gmail.com>
2024-08-11 19:07:12 +0200

    moved scraping-related files to seperate directory

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
-import requests
+from flask import Flask, render_template
 from scraping.scrape_sonderborg_dk import scrape_sonderborg_dk
 from scraping.scrape_visit_sonderjylland_dk import scrape_visitsonderjylland_dk
 from scraping.scrape_kulturisyd_dk import scrape_kulturisyd_dk
 from scraping.scrape_koncertsalenalison_dk import scrape_koncertsalenalsion_dk
 from event import Event
-from utils import print_events, print_events_long
+import requests
 
 SONDERBORG_DK_URL = "https://sonderborg.dk/begivenheder/"
 VISITSONDERJYYLAND_DK_URL = "https://www.visitsonderjylland.dk/sonderborg/det-sker/det-sker-i-soenderborg"
 KULTURISYD_DK_URL = "https://kulturisyd.dk"
 KONCERTSALENALISON_DK_URL = "https://koncertsalenalsion.dk/da/koncertsalen"
 
+app = Flask(__name__)
 
-def main():
-    # Your main program logic goes here
-    print("running main")
+
+@app.route('/')
+def index():
     all_events: list[Event] = []
 
     # sonderborg.dk
@@ -22,15 +23,12 @@ def main():
     response = requests.get(SONDERBORG_DK_URL)
     html_content = response.content
     sonderborg_dk_events: list[Event] = scrape_sonderborg_dk(html_content)
-    # print_events(sonderborg_dk_events)
-
     # visitsonderjyyland.dk
     print("Downloading the visitsonderjylland.dk data...")
     response = requests.get(VISITSONDERJYYLAND_DK_URL)
     html_content = response.content
     visitsonderjylland_dk_events: list[Event] = scrape_visitsonderjylland_dk(
         html_content)
-    # print_events(visitsonderjylland_dk_events)
 
     # kulturisyd.dk
     print("Downloading the kulturisyd.dk data...")
@@ -38,7 +36,6 @@ def main():
     html_content = response.content
     kulturisyd_dk_events: list[Event] = scrape_kulturisyd_dk(
         html_content, KULTURISYD_DK_URL)
-    # print_events(kulturisyd_dk_events)
 
     # koncertsalenalsion.dk
     print("Downloading the koncertsalenalsion.dk data...")
@@ -46,17 +43,18 @@ def main():
     html_content = response.content
     koncertsalenalison_dk_events: list[Event] = scrape_koncertsalenalsion_dk(
         html_content)
-    # print_events(koncertsalenalison_dk_events)
 
+    # Combine all the events into a single list
     all_events.extend(sonderborg_dk_events)
     all_events.extend(visitsonderjylland_dk_events)
     all_events.extend(kulturisyd_dk_events)
     all_events.extend(koncertsalenalison_dk_events)
 
+    # Sort the events based on the event time
     all_events.sort(key=lambda x: x.time)
 
-    print_events_long(all_events)
+    return render_template('index.html', events=all_events)
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
