e145cb9a4101cfdbcc596b35080c2326e5ac5e6a
Jan Musiol <jasiek.musiol@gmail.com>
2024-08-11 20:16:12 +0200

    specified port and host

--- app.py
+++ app.py
@@ -69,5 +69,7 @@ if __name__ == '__main__':
     #     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
     # )
     app.run(
+        host="0.0.0.0",
+        port=8000
         # debug=True
     )
